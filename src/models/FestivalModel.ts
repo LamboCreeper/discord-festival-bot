e235527ba1f8bb5b03a421fe66770dc53a69b3d9
LamboCreeper <12111454+LamboCreeper@users.noreply.github.com>
2024-04-17 17:51:04 +0100

    Update FestivalModel to have display properties

--- src/models/FestivalModel.ts
+++ src/models/FestivalModel.ts
@@ -1,6 +1,14 @@
 import { ModelOptions, Prop } from "@typegoose/typegoose";
 import { BaseModel } from "./BaseModel";
 
+class FestivalDisplayModel {
+	@Prop()
+	emoji?: string;
+
+	@Prop()
+	colour?: string;
+}
+
 @ModelOptions({ schemaOptions: { collection: "festivals" }})
 export class FestivalModel extends BaseModel {
 
@@ -15,4 +23,12 @@ export class FestivalModel extends BaseModel {
 
 	@Prop({ required: true, default: true })
 	accepting_submissions!: boolean;
+
+	@Prop({ required: true })
+	announcement_channel!: string;
+
+	@Prop()
+	display?: FestivalDisplayModel;
 }
+
+
